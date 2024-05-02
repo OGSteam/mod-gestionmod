ffb1b696c96881fec42430c525e95a07cfaffc6c
machine62 <28401480+machine62@users.noreply.github.com>
2024-05-02 16:14:14 +0200

    redesign part 1
    Header
    menu
    footer

--- gestion.php
+++ gestion.php
@@ -1,23 +1,21 @@
 <?php
 
 /**
-* gestion.php Fichier de gestion des différentes parties
-* @package Gestion MOD
-* @author Kal Nightmare
-* @update xaviernuma - 2012
-* @link http://www.ogsteam.fr/
-*/
+ * gestion.php Fichier de gestion des différentes parties
+ * @package Gestion MOD
+ * @author Kal Nightmare
+ * @update xaviernuma - 2012
+ * @link http://www.ogsteam.fr/
+ */
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-if (!defined('IN_SPYOGAME')) 
-{
+if (!defined('IN_SPYOGAME')) {
 	die("Hacking attempt");
 }
 
-$s_sql = "SELECT `active` FROM `".TABLE_MOD."` WHERE `action` = 'gestion' AND `active` = 1 LIMIT 1;";
+$s_sql = "SELECT `active` FROM `" . TABLE_MOD . "` WHERE `action` = 'gestion' AND `active` = 1 LIMIT 1;";
 
-if (!$db->sql_numrows($db->sql_query($s_sql)))
-{
+if (!$db->sql_numrows($db->sql_query($s_sql))) {
 	die("Hacking attempt");
 }
 
@@ -27,117 +25,104 @@ global $user_data;
 $s_html = '';
 
 // Récupération de la version
-$s_sql = "SELECT `version` FROM `".TABLE_MOD."` WHERE `action` = 'gestion';";
+$s_sql = "SELECT `version` FROM `" . TABLE_MOD . "` WHERE `action` = 'gestion';";
 $ta_resultat = $db->sql_fetch_assoc($db->sql_query($s_sql));
 
 $version = $ta_resultat['version'];
 $dir = "gestionmod";
 
 require_once("views/page_header.php");
-require_once("mod/".$dir."/function.php");
+require_once("mod/" . $dir . "/function.php");
 
-if($user_data["user_admin"] <> 1 && $user_data["user_coadmin"] <> 1)
-{
+if ($user_data["user_admin"] <> 1 && $user_data["user_coadmin"] <> 1) {
 	redirection("index.php?action=message&id_message=forbidden&info");
-} 
+}
 
-if(!isset($pub_subaction)) 
-{
+if (!isset($pub_subaction)) {
 	$pub_subaction = 'list';
 }
 
-$s_sql = "SELECT * FROM `".TABLE_MOD."` WHERE `action` = 'autoupdate' and `active` = 1; ";
+$s_sql = "SELECT * FROM `" . TABLE_MOD . "` WHERE `action` = 'autoupdate' and `active` = 1; ";
 $r_sql = $db->sql_query($s_sql);
+$isautoupdate = false;
 
-if ($db->sql_numrows($r_sql) > 0) 
-{
+if ($db->sql_numrows($r_sql) > 0) {
 	$n_colonnes = 5;
 	$row = $db->sql_fetch_assoc($r_sql);
-	$lien = 'mod/'.$row['root'].'/'.$row['link'];
-} 
-else 
-{
-	$n_colonnes = 4;
+	$lien = 'mod/' . $row['root'] . '/' . $row['link'];
+	$isautoupdate = true;
 }
 
-$n_taille_colonne = floor(100/$n_colonnes);
-
-$s_html .= '<table style="width:100%;">';
-$s_html .= 	'<tr style="text-align:center;"><td class="c" colspan="'.$n_colonnes.'">GESTION MOD</td></tr>';
-$s_html .= 	'<tr style="text-align:center;">';
+$activeTagList  = ($pub_subaction == 'list') ? 'active' : '';
+$activeTagGroup  = ($pub_subaction == 'group') ? 'active' : '';
+$activeTagMod  = ($pub_subaction == 'mod') ? 'active' : '';
 
 
 if ($pub_subaction <> 'list')
-{
-	$s_html .= '<td class="c" style="width:'.$n_taille_colonne.'%;"><a href="index.php?action=gestion&subaction=list" style="color:lime;"';
-}
-else
-{
-	$s_html .= '<th style="width:'.$n_taille_colonne.'%;"><a';
-}
-$s_html .= '>Liste MOD</a></td>';
+?>
 
-if ($pub_subaction <> 'group') 
-{
-	$s_html .= '<td class="c" style="width:'.$n_taille_colonne.'%;"><a href="index.php?action=gestion&subaction=group" style="color: lime;"';
-}
-else
-{ 
-	$s_html .= '<th style="width:'.$n_taille_colonne.'%;"><a';
-}
+<div class="ogspy-mod-header">
+	<h2>Gestion Mod</h2>
+</div>
 
-$s_html .= '>Gestion Groupes</a></td>';
+<div class="nav-page-menu">
+	<div class="nav-page-menu-item <?php echo $activeTagList; ?>">
+		<a class="nav-page-menu-link" href="index.php?action=gestion&amp;subaction=list">
+			Liste MOD
+		</a>
+	</div>
+	<div class="nav-page-menu-item   <?php echo $activeTagGroup; ?>">
+		<a class="nav-page-menu-link" href="index.php?action=gestion&amp;subaction=group">
+			Gestion Groupes
+		</a>
+	</div>
+	<div class="nav-page-menu-item  <?php echo $activeTagMod; ?> ">
+		<a class="nav-page-menu-link" href="index.php?action=gestion&amp;subaction=mod">
+			Renommeur de MOD
+		</a>
+	</div>
+	<div class="nav-page-menu-item   ">
+		<a class="nav-page-menu-link" href="index.php?action=administration&amp;subaction=mod">
+			Administration des mods
+		</a>
+	</div>
+	<?php if ($isautoupdate) : ?>
+		<div class="nav-page-menu-item   ">
+			<a class="nav-page-menu-link" href="index.php?action=gestion&amp;subaction=modUpdate">
+				AutoUpdate
+			</a>
+		</div>
+	<?php endif; ?>
+</div>
 
-if ($pub_subaction <> 'mod') 
-{
-	$s_html .= '<td class="c" style="width:'.$n_taille_colonne.'%;"><a href="index.php?action=gestion&subaction=mod" style="color: lime;"';
-}
-else 
-{
-	$s_html .= '<th style="width:'.$n_taille_colonne.'%;"><a';
-}
-$s_html .= '>Renommeur de MOD</a></td>';
 
 
-$s_html .= '<td class="c" style="width:'.$n_taille_colonne.'%;"><a href="index.php?action=administration&subaction=mod" style="color: lime;"';
-$s_html .= '>Administration des mods</a></td>';
 
-if ($n_colonnes == 5) 
-{
-	if ($pub_subaction <> 'modUpdate') 
-	{
-		$s_html .= '<td class="c" style="width:'.$n_taille_colonne.'%;"><a href="index.php?action=gestion&subaction=modUpdate" style="color: lime;"';
-	}
-	else 
-	{
-		$s_html .= '<th style="width:'.$n_taille_colonne.'%;"><a';
-	}
-	$s_html .= '>AutoUpdate</a></td>';
-}
 
-$s_html .= 	'</tr>';
-$s_html .= '</table>';
 
-echo $s_html;
+<?php
 
-switch ($pub_subaction) 
-{
+
+
+
+
+switch ($pub_subaction) {
 	case 'list':
-		require('mod/'.$dir.'/list.php');
+		require('mod/' . $dir . '/list.php');
 		break;
 
 	case 'group':
-		require('mod/'.$dir.'/group.php');
+		require('mod/' . $dir . '/group.php');
 		break;
 
 	case 'mod':
-		require('mod/'.$dir.'/rename.php');
+		require('mod/' . $dir . '/rename.php');
 		break;
 
 	case 'modUpdate':
 		require($lien);
 		break;
-		
+
 	case 'new_group':
 		f_nouveau_groupe();
 		break;
@@ -145,24 +130,26 @@ switch ($pub_subaction)
 	case 'action_group':
 		f_gerer_un_groupe();
 		break;
-	case 'action_mod' :
+	case 'action_mod':
 		f_gerer_mod();
 		break;
-		
+
 	default:
-		require('mod/'.$dir.'/list.php');
+		require('mod/' . $dir . '/list.php');
 }
-
-if($pub_subaction <> 'modUpdate')
-{
-	$s_html = '';
-	$s_html .= '<div style="font-size:10px;width:400px;text-align:center;background-image:url(\'skin/OGSpy_skin/tableaux/th.png\');background-repeat:repeat;">Gestion MOD ('.$version.')';
-	$s_html .= '<br>Développé par <a href="mailto:kalnightmare@free.fr">Kal Nightmare</a> 2006';
-	$s_html .= '<br>Mise à jour par <a href="mailto:contact@epe-production.org?subject=gestionmod">xaviernuma</a> 2015</div>';
-
-	echo $s_html;
-}
-
-require_once("views/page_tail.php");
 
 ?>
+<?php if ($pub_subaction <> 'modUpdate') : ?>
+	<div class="ogspy-mod-footer">
+		<p>
+			Gestion MOD (<?php echo $version; ?>)
+			<br>Développé par <a href="mailto:kalnightmare@free.fr">Kal Nightmare</a> 2006 /
+			Mise à jour par <a href="mailto:contact@epe-production.org?subject=gestionmod">xaviernuma</a> 2015
+	</div>
+	</p>
+	</div>
+<?php endif; ?>
+
+
+<?php
+require_once("views/page_tail.php");
